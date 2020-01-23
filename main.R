311797de012bbb4f809b0a42982f2a311bafc8ab
pimz53 <apimenta@performetric.net>
2020-01-23 21:32:58 +0000

    h2o automl basic script added

--- main.R
+++ main.R
@@ -1,5 +1,35 @@
 library(h2o)
-localH2O = h2o.init()
 
-train <- read.csv(file="dataset/train.csv", header=TRUE, sep=",")
-test <- read.csv(file="dataset/test.csv", header=TRUE, sep=",")
+h2o.init()
+
+train <- h2o.importFile(file="dataset/train.csv")
+test <- h2o.importFile(file="dataset/test.csv")
+
+# Identify predictors and response
+y <- "SalePrice"
+x <- setdiff(names(train), y)
+
+# For binary classification, response should be a factor
+train[,y] <- as.factor(train[,y])
+test[,y] <- as.factor(test[,y])
+
+aml <- h2o.automl(x = x, y = y,
+                  training_frame = train,
+                  leaderboard_frame = test,
+                  max_runtime_secs = 30)
+
+# View the AutoML Leaderboard
+lb <- aml@leaderboard
+lb
+
+aml@leader
+
+
+# If you need to generate predictions on a test set, you can make
+# predictions directly on the `"H2OAutoML"` object, or on the leader
+# model object directly
+
+#pred <- h2o.predict(aml, test)  #Not functional yet: https://0xdata.atlassian.net/browse/PUBDEV-4428
+
+# or:
+pred <- h2o.predict(aml@leader, test)
