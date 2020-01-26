c672c5d81d066ce8635914d410f41bec899923e5
pimz53 <apimenta@performetric.net>
2020-01-26 00:11:45 +0000

    using nfolds insted of validation frame

--- main.R
+++ main.R
@@ -1,38 +1,34 @@
 library(h2o)
 
-h2o.init()
+h2o.init(ip="localhost", port = 54131)
 
 train_csv <- read.csv("dataset/train.csv")
 test_csv <- read.csv("dataset/test.csv")
 
-row_count <- nrow(train_csv)
-shuffled_rows <- sample(row_count)
-train <- train_csv[head(shuffled_rows,floor(row_count*0.75)),]
-valid <- train_csv[tail(shuffled_rows,floor(row_count*0.25)),]
+#row_count <- nrow(train_csv)
+#shuffled_rows <- sample(row_count)
+#train <- train_csv[head(shuffled_rows,floor(row_count*0.75)),]
+#valid <- train_csv[tail(shuffled_rows,floor(row_count*0.25)),]
 
 
 train <- as.h2o(train)
-valid <- as.h2o(valid)
+#valid <- as.h2o(valid)
 test <- as.h2o(test_csv)
 
 # Identify predictors and response
 y <- "SalePrice"
-x <- setdiff(names(train), y)
+ignore_fields <- ("Id","MiscVal","Fireplaces")
+x <- setdiff(names(train), ignore_fields)
 
-# For binary classification, response should be a factor
-train[,y] <- as.factor(train[,y])
-valid[,y] <- as.factor(valid[,y])
 
 aml <- h2o.automl(x = x,
                      y = y,
                      training_frame = train,
                      nfolds = 5,
-                     stopping_tolerance = 0.005,
-                     sort_metric = "logloss",
-                     stopping_metric = "logloss",
+                     #stopping_tolerance = 0.003,
+                     sort_metric = "RMSE",
                      stopping_rounds = 3,
-                     exclude_algos = c("GLM","DeepLearning","XGBoost","DRF","StackedEnsemble"),
-                     max_runtime_secs = 30,
+                     exclude_algos = c("DRF","StackedEnsemble"),
                      seed = 1234)
 
 # View the AutoML Leaderboard
@@ -42,4 +38,4 @@ lb
 aml@leader
 
 
-pred <- h2o.predict(aml@leader, valid)
+#pred <- h2o.predict(aml@leader, valid)
